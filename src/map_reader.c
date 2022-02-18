779596785a903f8dd53b3ffe0ce366e2daad5d8a
Simon Wautelet <swautele@fu-c1-p3.s19.be>
2022-02-18 19:40:12 +0100

    autosave

--- src/map_reader.c
+++ src/map_reader.c
@@ -1,17 +1,58 @@
 #include "so_long.h"
 
-int	map_reader(char *name)
+char	**map_reader(char *name)
 {
 	int		fd;
 	char	*line;
-	ssize_t	num;
+	size_t	height;
+	size_t	len;
+	char	**map;
 
+	high = 1;
 	fd = open (name, O_RDONLY);
-	line = "";
+	line = get_next_line(fd);
+	len = ft_strlen(line);
 	while (line)
 	{
 	line = get_next_line(fd);
+	if (len != ft_strlen(line))
+		{
+			printf("error\nthe map is not a rectangle");
+			return (NULL);
+		}
+	height++;
 	}
 	close (fd);
-	return (0);
+	if (height < 3 || len < 3)
+	{
+		printf("error\nthe map is too little");
+		return (NULL);
+	}
+	map = map_translate(len, height, name);
+	return (map);
+}
+
+char	**map_translate(const int len, const int height, char *name)
+{
+	int		fd;
+	char 	map[len][height];
+	int		i;
+	int		j;
+	char 	*line;
+
+	i = 0;
+	j = 0;
+	fd = open(name, O_RDONLY);
+	while (j < height)
+	{
+		line = get_next_line(fd);
+		while (i < len)
+		{
+			map[i][j] = line[i];
+			i++;
+		}
+		j++;
+		i = 0;
+	}
+	close(fd);
 }
