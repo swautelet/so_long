b17fa043371a19cbd4acbce89f912658f72bea4d
Simon Wautelet <swautele@fu-c1-p3.s19.be>
2022-02-22 21:10:43 +0100

    autosave

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 13:03:30 by swautele          #+#    #+#             */
-/*   Updated: 2022/02/22 20:19:19 by swautele         ###   ########.fr       */
+/*   Updated: 2022/02/22 21:06:23 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ int	main(int argc, char **argv)
 		return (-1);
 	map = map_reader(argv[1]);
 	if (map_error(map) == -1)
+	{
+		ft_lstclear(&map, &free);
 		return (-1);
+	}
 	next = map;
 	printf("%s", next->content);
 	while(next->next)
@@ -29,6 +32,7 @@ int	main(int argc, char **argv)
 		next = next->next;
 		printf("%s", next->content);
 	}
+	printf("%s", (ft_lstlast(map))->content);
 	ft_lstclear(&map, &free);
 	return (0);
 }
