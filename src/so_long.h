7a1eac9009c09fe7d66a65c77ded10dd52bdbcd6
SWautele <swautele@student.s19.be>
2022-03-02 17:45:00 +0100

    autosave

--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 13:04:20 by swautele          #+#    #+#             */
-/*   Updated: 2022/03/02 17:02:07 by swautele         ###   ########.fr       */
+/*   Updated: 2022/03/02 17:15:31 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@
 #  define BUFFER_SIZE 1000
 # endif
 
-# define ANIM_WAIT 100000
+# define ANIM_WAIT 250000
 
 enum e_dir
 {
