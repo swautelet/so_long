58b23a57069b1172083de9944f8d7f63b9ae6e7f
SWautele <swautele@student.s19.be>
2022-03-03 18:14:28 +0100

    refactor animate.c + NORM

--- src/animate.c
+++ src/animate.c
@@ -6,108 +6,65 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 17:11:16 by swautele          #+#    #+#             */
-/*   Updated: 2022/03/03 17:36:58 by swautele         ###   ########.fr       */
+/*   Updated: 2022/03/03 18:14:13 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static void	sub_animate_end(t_img *dt)
+{
+	if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
+	{
+		printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
+		ft_exit(dt);
+	}
+	else
+		printmove(dt);
+	if (dt->dir == DIR_RIGHT || dt->dir == DIR_LEFT
+		|| dt->dir == DIR_DOWN2 || dt->dir == DIR_UP2)
+		dt->dir = PAUSE;
+	if (dt->dir == DIR_UP)
+		dt->dir = DIR_UP2;
+	if (dt->dir == DIR_DOWN)
+		dt->dir = DIR_DOWN2;
+}
+
+static void	sub_animate(t_img *dt, int off, void *player, int posy_inc)
+{
+	usleep(ANIM_WAIT);
+	mlx_put_image_to_window(dt->video, dt->win, dt->floor,
+		(dt->pos_x - off) * dt->siz, dt->pos_y * dt->siz);
+	if ((dt->pos_x - off) == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+		mlx_put_image_to_window(dt->video, dt->win, dt->door,
+			(dt->pos_x - off) * dt->siz, dt->pos_y * dt->siz);
+	mlx_put_image_to_window(dt->video, dt->win, dt->floor,
+		dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+	if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+		mlx_put_image_to_window(dt->video, dt->win, dt->door,
+			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+	dt->pos_y += posy_inc;
+	mlx_put_image_to_window(dt->video, dt->win, player,
+		dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+	dt->move++;
+	sub_animate_end(dt);
+}
+
 int	animate(t_img *dt)
 {
 	if (dt->dir == DIR_RIGHT)
-	{
-		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor, (dt->pos_x - 1) * dt->siz, dt->pos_y * dt->siz);
-		if ((dt->pos_x - 1) == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door, (dt->pos_x - 1) * dt->siz, dt->pos_y * dt->siz);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		mlx_put_image_to_window(dt->video, dt->win, dt->player_r, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		dt->move++;
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
-		{
-			printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
-			ft_exit(dt);
-		}
-		else
-			printmove(dt);
-		dt->dir = PAUSE;
-	}
-	else if (dt->dir == DIR_DOWN)
-	{
-		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		dt->pos_y++;
-		mlx_put_image_to_window(dt->video, dt->win, dt->player_f2, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		dt->dir = DIR_DOWN2;
-	}
-	else if (dt->dir == DIR_DOWN2)
-	{
-		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		mlx_put_image_to_window(dt->video, dt->win, dt->player, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		dt->move++;
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
-		{
-			printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
-			ft_exit(dt);
-		}
-		else
-			printmove(dt);
-		dt->dir = PAUSE;
-	}
+		sub_animate(dt, 1, dt->player_r, 0);
 	else if (dt->dir == DIR_LEFT)
-	{
-		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor, (dt->pos_x + 1) * dt->siz, dt->pos_y * dt->siz);
-		if ((dt->pos_x + 1) == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door, (dt->pos_x + 1) * dt->siz, dt->pos_y * dt->siz);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		mlx_put_image_to_window(dt->video, dt->win, dt->player_l, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		dt->move++;
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
-		{
-			printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
-			ft_exit(dt);
-		}
-		else
-			printmove(dt);
-		dt->dir = PAUSE;
-	}
+		sub_animate(dt, -1, dt->player_l, 0);
 	else if (dt->dir == DIR_UP)
-	{
-		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		dt->pos_y--;
-		mlx_put_image_to_window(dt->video, dt->win, dt->player_b2, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		dt->dir = DIR_UP2;
-	}
+		sub_animate(dt, 0, dt->player_b2, -1);
+	else if (dt->dir == DIR_DOWN)
+		sub_animate(dt, 0, dt->player_f2, 1);
+	else if (dt->dir == DIR_DOWN2)
+		sub_animate(dt, 0, dt->player, 0);
 	else if (dt->dir == DIR_UP2)
-	{
-		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		mlx_put_image_to_window(dt->video, dt->win, dt->player_b, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		dt->move++;
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
-		{
-			printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
-			ft_exit(dt);
-		}
-		else
-			printmove(dt);
-		dt->dir = PAUSE;
-	}
-	dt->done = 0;
+		sub_animate(dt, 0, dt->player_b, 0);
+	if (dt->dir != DIR_UP && dt->dir != DIR_DOWN)
+		dt->done = 0;
 	return (0);
 }
