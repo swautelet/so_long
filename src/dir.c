e0873ae20713c3335c0ba58b0c35a4cda6704638
SWautele <swautele@student.s19.be>
2022-03-03 19:06:08 +0100

    autosave

--- src/dir.c
+++ src/dir.c
@@ -6,11 +6,20 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 19:00:45 by swautele          #+#    #+#             */
-/*   Updated: 2022/03/03 19:01:12 by swautele         ###   ########.fr       */
+/*   Updated: 2022/03/03 19:05:44 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static void	erase(t_img *dt)
+{
+	mlx_put_image_to_window(dt->video, dt->win, dt->floor,
+		dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+	if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+		mlx_put_image_to_window(dt->video, dt->win, dt->door,
+			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+}
 
 void	move_up(t_img *dt)
 {
@@ -19,11 +28,7 @@ void	move_up(t_img *dt)
 	check = ft_authorized(dt, 0, -1);
 	if (check == 0)
 	{
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor,
-			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door,
-				dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		erase(dt);
 		mlx_put_image_to_window(dt->video, dt->win, dt->player_b1,
 			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
 		dt->dir = DIR_UP;
@@ -31,11 +36,7 @@ void	move_up(t_img *dt)
 	if (check == 2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor,
-			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door,
-				dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		erase(dt);
 		dt->pos_y--;
 		mlx_put_image_to_window(dt->video, dt->win, dt->player_p_b,
 			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
@@ -50,11 +51,7 @@ void	move_left(t_img *dt)
 	check = ft_authorized(dt, -1, 0);
 	if (check == 0)
 	{
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor,
-			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door,
-				dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		erase(dt);
 		mlx_put_image_to_window(dt->video, dt->win, dt->player_l1,
 			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
 		dt->dir = DIR_LEFT;
@@ -63,11 +60,7 @@ void	move_left(t_img *dt)
 	if (check == 2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor,
-			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door,
-				dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		erase(dt);
 		mlx_put_image_to_window(dt->video, dt->win, dt->player_p_l,
 			(dt->pos_x - 1) * 64, dt->pos_y * 64);
 		dt->dir = DIR_LEFT;
@@ -82,11 +75,7 @@ void	move_down(t_img *dt)
 	check = ft_authorized(dt, 0, 1);
 	if (check == 0)
 	{
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor,
-			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door,
-				dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		erase(dt);
 		mlx_put_image_to_window(dt->video, dt->win, dt->player_f1,
 			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
 		dt->dir = DIR_DOWN;
@@ -94,11 +83,7 @@ void	move_down(t_img *dt)
 	if (check == 2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor,
-			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door,
-				dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		erase(dt);
 		dt->pos_y++;
 		mlx_put_image_to_window(dt->video, dt->win, dt->player_p_f,
 			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
@@ -113,11 +98,7 @@ void	move_right(t_img *dt)
 	check = ft_authorized(dt, 1, 0);
 	if (check == 0)
 	{
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor,
-			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door,
-				dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		erase(dt);
 		mlx_put_image_to_window(dt->video, dt->win, dt->player_r1,
 			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
 		dt->dir = DIR_RIGHT;
@@ -126,11 +107,7 @@ void	move_right(t_img *dt)
 	if (check == 2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(dt->video, dt->win, dt->floor,
-			dt->pos_x * dt->siz, dt->pos_y * dt->siz);
-		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
-			mlx_put_image_to_window(dt->video, dt->win, dt->door,
-				dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		erase(dt);
 		mlx_put_image_to_window(dt->video, dt->win, dt->player_p_r,
 			(dt->pos_x + 1) * 64, dt->pos_y * 64);
 		dt->dir = DIR_RIGHT;
