35b439ec3d724352fd56f4f7a1dec7b4ed554a2d
SWautele <swautele@student.s19.be>
2022-03-03 18:53:36 +0100

    autosave

--- src/map_reader.c
+++ src/map_reader.c
@@ -6,7 +6,7 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 13:02:17 by swautele          #+#    #+#             */
-/*   Updated: 2022/03/03 18:50:48 by swautele         ###   ########.fr       */
+/*   Updated: 2022/03/03 18:52:59 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,29 @@
 
 t_list	*map_reader(char *name)
 {
-	t_reader	*read;
-	t_reader	fuck;
+	t_reader	read;
 
-	read = &fuck;
-	read->i = 0;
-	read->fd = open(name, O_RDONLY);
-	read->line = get_next_line(read->fd);
-	read->map = ft_lstnew(read->line);
-	read->map->line = read->i;
-	while (read->line)
+	read.i = 0;
+	read.fd = open(name, O_RDONLY);
+	read.line = get_next_line(read.fd);
+	read.map = ft_lstnew(read.line);
+	read.map->line = read.i;
+	while (read.line)
 	{
-		read->line = get_next_line(read->fd);
-		read->i++;
-		if (read->line != NULL)
+		read.line = get_next_line(read.fd);
+		read.i++;
+		if (read.line != NULL)
 		{
-			read->new = ft_lstnew(read->line);
-			if (read->new == NULL)
+			read.new = ft_lstnew(read.line);
+			if (read.new == NULL)
 			{
-				free(read->line);
-				return (read->map);
+				free(read.line);
+				return (read.map);
 			}
-			read->new->line = read->i;
-			ft_lstadd_back(&read->map, read->new);
+			read.new->line = read.i;
+			ft_lstadd_back(&read.map, read.new);
 		}
 	}
-	close(read->fd);
-	return (read->map);
+	close(read.fd);
+	return (read.map);
 }
