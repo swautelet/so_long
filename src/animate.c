7111c6a1099ef38052f95861e4db34000a6accb5
SWautele <swautele@student.s19.be>
2022-03-03 16:48:53 +0100

    autosave

--- src/animate.c
+++ src/animate.c
@@ -6,109 +6,105 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 17:11:16 by swautele          #+#    #+#             */
-/*   Updated: 2022/03/03 16:22:42 by swautele         ###   ########.fr       */
+/*   Updated: 2022/03/03 16:48:08 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	animate(t_img *sprite)
+int	animate(t_img *dt)
 {
-	if (sprite->dir == DIR_RIGHT)
+	if (dt->dir == DIR_RIGHT)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->pos_x++;
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_r, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->move++;
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y && sprite->flag == 0)
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, (dt->pos_x - 1) * dt->siz, dt->pos_y * dt->siz);
+		if ((dt->pos_x - 1) == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, (dt->pos_x - 1) * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_r, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->move++;
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
 		{
-			printf("You won in %d moves GG.\nYou can walk Loki now.\n", sprite->move);
-			ft_exit(sprite);
+			printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
+			ft_exit(dt);
 		}
 		else
-			printmove(sprite);
-		sprite->dir = PAUSE;
+			printmove(dt);
+		dt->dir = PAUSE;
 	}
-	else if (sprite->dir == DIR_DOWN)
+	else if (dt->dir == DIR_DOWN)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->pos_y++;
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_f2, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->dir = DIR_DOWN2;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, (dt->pos_y - 1) * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && (dt->pos_y - 1) == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, (dt->pos_y - 1) * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_f2, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->dir = DIR_DOWN2;
 	}
-	else if (sprite->dir == DIR_DOWN2)
+	else if (dt->dir == DIR_DOWN2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->move++;
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y && sprite->flag == 0)
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->move++;
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
 		{
-			printf("You won in %d moves GG.\nYou can walk Loki now.\n", sprite->move);
-			ft_exit(sprite);
+			printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
+			ft_exit(dt);
 		}
 		else
-			printmove(sprite);
-		sprite->dir = PAUSE;
+			printmove(dt);
+		dt->dir = PAUSE;
 	}
-	else if (sprite->dir == DIR_LEFT)
+	else if (dt->dir == DIR_LEFT)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->pos_x--;
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_l, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->move++;
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y && sprite->flag == 0)
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, (dt->pos_x + 1) * dt->siz, dt->pos_y * dt->siz);
+		if ((dt->pos_x + 1) == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, (dt->pos_x + 1) * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_l, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->move++;
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
 		{
-			printf("You won in %d moves GG.\nYou can walk Loki now.\n", sprite->move);
-			ft_exit(sprite);
+			printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
+			ft_exit(dt);
 		}
 		else
-			printmove(sprite);
-		sprite->dir = PAUSE;
+			printmove(dt);
+		dt->dir = PAUSE;
 	}
-	else if (sprite->dir == DIR_UP)
+	else if (dt->dir == DIR_UP)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->pos_y--;
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_b2, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->dir = DIR_UP2;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, (dt->pos_y + 1) * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && (dt->pos_y + 1) == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, (dt->pos_y + 1) * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_b2, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->dir = DIR_UP2;
 	}
-	else if (sprite->dir == DIR_UP2)
+	else if (dt->dir == DIR_UP2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_b, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->move++;
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y && sprite->flag == 0)
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_b, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->move++;
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y && dt->flag == 0)
 		{
-			printf("You won in %d moves GG.\nYou can walk Loki now.\n", sprite->move);
-			ft_exit(sprite);
+			printf("You won in %d moves GG.\nYou can walk Loki now.\n", dt->move);
+			ft_exit(dt);
 		}
 		else
-			printmove(sprite);
-		sprite->dir = PAUSE;
+			printmove(dt);
+		dt->dir = PAUSE;
 	}
 	return (0);
 }
--- src/keyboard.c
+++ src/keyboard.c
@@ -6,154 +6,163 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 17:31:31 by swautele          #+#    #+#             */
-/*   Updated: 2022/03/03 16:22:42 by swautele         ###   ########.fr       */
+/*   Updated: 2022/03/03 16:48:23 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	keyplan(int keycode, t_img *sprite)
+int	keyplan(int keycode, t_img *dt)
 {
 	if (keycode == 53)
-		ft_exit(sprite);
-	if (keycode == 13 && sprite->pos_y > 1)
-		move_up(sprite);
-	if (keycode == 0 && sprite->pos_x > 0)
-		move_left(sprite);
+		ft_exit(dt);
+	if (keycode == 13 && dt->pos_y > 1)
+		move_up(dt);
+	if (keycode == 0 && dt->pos_x > 0)
+		move_left(dt);
 	if (keycode == 1)
-		move_down(sprite);
+		move_down(dt);
 	if (keycode == 2)
-		move_right(sprite);
+		move_right(dt);
 	return (0);
 }
 
-int	ft_exit(t_img *sprite)
+int	ft_exit(t_img *dt)
 {
-	ft_lstclear(&sprite->map, free);
-	mlx_destroy_image(sprite->video, sprite->leash);
-	mlx_destroy_image(sprite->video, sprite->collar);
-	mlx_destroy_image(sprite->video, sprite->boot);
-	mlx_destroy_image(sprite->video, sprite->poopbag);
-	mlx_destroy_image(sprite->video, sprite->key);
-	mlx_destroy_image(sprite->video, sprite->player);
-	mlx_destroy_image(sprite->video, sprite->loki);
-	mlx_destroy_image(sprite->video, sprite->door);
-	mlx_destroy_image(sprite->video, sprite->door_o);
-	mlx_destroy_image(sprite->video, sprite->opal);
-	mlx_destroy_image(sprite->video, sprite->player_b);
-	mlx_destroy_image(sprite->video, sprite->player_b1);
-	mlx_destroy_image(sprite->video, sprite->player_b2);
-	mlx_destroy_image(sprite->video, sprite->player_f2);
-	mlx_destroy_image(sprite->video, sprite->player_f1);
-	mlx_destroy_image(sprite->video, sprite->player_l);
-	mlx_destroy_image(sprite->video, sprite->player_l1);
-	mlx_destroy_image(sprite->video, sprite->player_p_b);
-	mlx_destroy_image(sprite->video, sprite->player_p_f);
-	mlx_destroy_image(sprite->video, sprite->player_p_r);
-	mlx_destroy_image(sprite->video, sprite->player_p_l);
-	mlx_destroy_image(sprite->video, sprite->player_r);
-	mlx_destroy_image(sprite->video, sprite->player_r1);
-	mlx_destroy_image(sprite->video, sprite->wall);
-	mlx_destroy_image(sprite->video, sprite->floor);
-	mlx_destroy_window(sprite->video, sprite->win);
-	free(sprite);
+	ft_lstclear(&dt->map, free);
+	mlx_destroy_image(dt->video, dt->leash);
+	mlx_destroy_image(dt->video, dt->collar);
+	mlx_destroy_image(dt->video, dt->boot);
+	mlx_destroy_image(dt->video, dt->poopbag);
+	mlx_destroy_image(dt->video, dt->key);
+	mlx_destroy_image(dt->video, dt->player);
+	mlx_destroy_image(dt->video, dt->loki);
+	mlx_destroy_image(dt->video, dt->door);
+	mlx_destroy_image(dt->video, dt->door_o);
+	mlx_destroy_image(dt->video, dt->opal);
+	mlx_destroy_image(dt->video, dt->player_b);
+	mlx_destroy_image(dt->video, dt->player_b1);
+	mlx_destroy_image(dt->video, dt->player_b2);
+	mlx_destroy_image(dt->video, dt->player_f2);
+	mlx_destroy_image(dt->video, dt->player_f1);
+	mlx_destroy_image(dt->video, dt->player_l);
+	mlx_destroy_image(dt->video, dt->player_l1);
+	mlx_destroy_image(dt->video, dt->player_p_b);
+	mlx_destroy_image(dt->video, dt->player_p_f);
+	mlx_destroy_image(dt->video, dt->player_p_r);
+	mlx_destroy_image(dt->video, dt->player_p_l);
+	mlx_destroy_image(dt->video, dt->player_r);
+	mlx_destroy_image(dt->video, dt->player_r1);
+	mlx_destroy_image(dt->video, dt->wall);
+	mlx_destroy_image(dt->video, dt->floor);
+	mlx_destroy_window(dt->video, dt->win);
+	free(dt);
 	exit(0);
 }
 
-void	move_up(t_img *sprite)
+void	move_up(t_img *dt)
 {
 	int	check;
 
-	check = ft_authorized(sprite, DIR_UP);
+	check = ft_authorized(dt, DIR_UP);
 	if (check == 0)
 	{
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_b1, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->dir = DIR_UP;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_b1, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->dir = DIR_UP;
+		dt->pos_y--;
 	}
 	if (check == 2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->pos_y--;
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_p_b, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->dir = DIR_UP2;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->pos_y--;
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_p_b, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->dir = DIR_UP2;
+		dt->pos_y--;
 	}
 }
 
-void	move_left(t_img *sprite)
+void	move_left(t_img *dt)
 {
 	int	check;
 
-	check = ft_authorized(sprite, DIR_LEFT);
+	check = ft_authorized(dt, DIR_LEFT);
 	if (check == 0)
 	{
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_l1, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->dir = DIR_LEFT;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_l1, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->dir = DIR_LEFT;
+		dt->pos_x--;
+
 	}
 	if (check == 2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_p_l, (sprite->pos_x - 1) * 64, sprite->pos_y * 64);
-		sprite->dir = DIR_LEFT;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_p_l, (dt->pos_x - 1) * 64, dt->pos_y * 64);
+		dt->dir = DIR_LEFT;
+		dt->pos_x--;
 	}
 }
 
-void	move_down(t_img *sprite)
+void	move_down(t_img *dt)
 {
 	int	check;
 
-	check = ft_authorized(sprite, DIR_DOWN);
+	check = ft_authorized(dt, DIR_DOWN);
 	if (check == 0)
 	{
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_f1, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->dir = DIR_DOWN;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_f1, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->dir = DIR_DOWN;
+		dt->pos_y++;
 	}
 	if (check == 2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->pos_y++;
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_p_f, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->dir = DIR_DOWN2;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->pos_y++;
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_p_f, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->dir = DIR_DOWN2;
+		dt->pos_y++;
 	}
 }
 
-void	move_right(t_img *sprite)
+void	move_right(t_img *dt)
 {
 	int	check;
 
-	check = ft_authorized(sprite, DIR_RIGHT);
+	check = ft_authorized(dt, DIR_RIGHT);
 	if (check == 0)
 	{
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_r1, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		sprite->dir = DIR_RIGHT;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_r1, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		dt->dir = DIR_RIGHT;
+		dt->pos_x++;
 	}
 	if (check == 2)
 	{
 		usleep(ANIM_WAIT);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->floor, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		if (sprite->pos_x == sprite->pos_d_x && sprite->pos_y == sprite->pos_d_y)
-			mlx_put_image_to_window(sprite->video, sprite->win, sprite->door, sprite->pos_x * sprite->siz, sprite->pos_y * sprite->siz);
-		mlx_put_image_to_window(sprite->video, sprite->win, sprite->player_p_r, (sprite->pos_x + 1) * 64, sprite->pos_y * 64);
-		sprite->dir = DIR_RIGHT;
+		mlx_put_image_to_window(dt->video, dt->win, dt->floor, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		if (dt->pos_x == dt->pos_d_x && dt->pos_y == dt->pos_d_y)
+			mlx_put_image_to_window(dt->video, dt->win, dt->door, dt->pos_x * dt->siz, dt->pos_y * dt->siz);
+		mlx_put_image_to_window(dt->video, dt->win, dt->player_p_r, (dt->pos_x + 1) * 64, dt->pos_y * 64);
+		dt->dir = DIR_RIGHT;
+		dt->pos_x++;
 	}
 }
