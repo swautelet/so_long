99cc8e5e2334b9114bb3d26064fb9143ed0827c0
SWautele <swautele@student.s19.be>
2022-03-03 20:08:57 +0100

    autosave

--- src/error.c
+++ src/error.c
@@ -6,11 +6,17 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 13:03:08 by swautele          #+#    #+#             */
-/*   Updated: 2022/03/03 19:27:30 by swautele         ###   ########.fr       */
+/*   Updated: 2022/03/03 20:08:03 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static int	error_msg(char *str)
+{
+	write(1, str, ft_strlen(str));
+	return (-1);
+}
 
 int	ft_error(int argc, char **argv)
 {
@@ -18,20 +24,24 @@ int	ft_error(int argc, char **argv)
 
 	ext = ".ber";
 	if (argc < 2)
-	{
-		printf("error\nTheres's no map");
-		return (-1);
-	}
+		return (error_msg("error\nTheres's no map\n"));
 	if (argc > 2)
-	{
-		printf("error\nthere's too much map");
-		return (-1);
-	}
+		return (error_msg("error\nthere's too much map\n"));
 	if (ft_strncmp(ft_strnstr(argv[1], ".ber", SIZE_T_MAX), ext, 4) != 0)
-	{
-		printf("error\nthe map is in the wrong format");
-		return (-1);
-	}
+		return (error_msg("error\nthe map is in the wrong format\n"));
+	return (0);
+}
+
+static int	count_error(t_check check)
+{
+	if (check.len < 3 || check.height < 3)
+		return (error_msg("Error\nthe map is too short\n"));
+	if (check.player != 1)
+		return (error_msg("Error\nthere should be only one player\n"));
+	if (check.exit != 1)
+		return (error_msg("Error\nthere should be only one exit\n"));
+	if (check.collectible == 0)
+		return (error_msg("Error\nthere is no collectible\n"));
 	return (0);
 }
 
@@ -47,10 +57,7 @@ int	map_error(t_list *map)
 	while (map->content[check.i] != '\n')
 	{
 		if (map->content[check.i] != '1')
-		{
-			printf("error\nthere miss a wall around the map");
-			return (-1);
-		}
+			return (error_msg("error\nthere miss a wall around the map\n"));
 		check.i++;
 	}
 	check.height = 1;
@@ -69,46 +76,17 @@ int	map_error(t_list *map)
 			check.i++;
 		}
 		if (ft_strlen(map->content) != check.len)
-		{
-			printf("Error\nthe map is not a rectangle");
-			return (-1);
-		}
+			return (error_msg("Error\nthe map is not a rectangle\n"));
 		if (map->content[0] != '1' || map->content[check.len - 2] != '1')
-		{
-			printf("Error\nthere miss a wall around the map");
-			return (-1);
-		}
+			return (error_msg("Error\nthere miss a wall around the map\n"));
 		check.height++;
 	}
 	check.i = 0;
 	while (map->content[check.i] != '\n')
 	{
 		if (map->content[check.i] != '1')
-		{
-			printf("Error\nthere miss a wall around the map");
-			return (-1);
-		}
+			return (error_msg("Error\nthere miss a wall around the map\n"));
 		check.i++;
 	}
-	if (check.len < 3 || check.height < 3)
-	{
-		printf("Error\nthe map is too short");
-		return (-1);
-	}
-	if (check.player != 1)
-	{
-		printf("Error\nthere should be only one player");
-		return (-1);
-	}
-	if (check.exit != 1)
-	{
-		printf("Error\nthere should be only one exit");
-		return (-1);
-	}
-	if (check.collectible == 0)
-	{
-		printf("Error\nthere is no collectible");
-		return (-1);
-	}
-	return (0);
+	return (count_error(check));
 }
